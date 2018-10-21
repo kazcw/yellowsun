258cd8dfb8992a68a7ac2eaf487d93e5177be3cb
Kaz Wesley <kaz@lambdaverse.org>
2018-10-21 15:51:31 -0700

    remove debugging noise

--- src/cn_aesni.rs
+++ src/cn_aesni.rs
@@ -56,8 +56,6 @@ impl Default for Cnv2 {
 }
 
 unsafe fn int_sqrt_v2(input: u64) -> u64 {
-    //let r = ((input as f64 + 18446744073709551616.0).sqrt() * 2.0 - 8589934592.0) as u64;
-
     let r = {
         let exp_double_bias = _mm_set_epi64x(0, (1023u64 << 52) as i64);
         let x = std::mem::transmute(_mm_add_epi64(_mm_cvtsi64_si128((input >> 12) as i64), exp_double_bias));
@@ -129,16 +127,6 @@ impl Variant for Cnv2 {
     fn mem_size() -> u32 { 0x20_0000 }
 }
 
-const DEBUG_ITERS: u32 = 3;
-
-fn print_line(mem: &mut [__m128i], j: u32) {
-    let j = j as usize;
-    eprintln!("[j]={:x?}", mem[j]);
-    eprintln!("[j ^ 1]={:x?}", mem[j ^ 1]);
-    eprintln!("[j ^ 2]={:x?}", mem[j ^ 2]);
-    eprintln!("[j ^ 3]={:x?}", mem[j ^ 3]);
-}
-
 #[target_feature(enable = "aes", enable = "sse4.1", enable = "sse4.2")]
 unsafe fn mix_inner<V: Variant>(mem: &mut [__m128i], from: &[__m128i], mut var: V) {
     let mut aa = _mm_xor_si128(from[0], from[2]);
@@ -146,47 +134,23 @@ unsafe fn mix_inner<V: Variant>(mem: &mut [__m128i], from: &[__m128i], mut var: 
     let mut a0 = _mm_extract_epi64(aa, 0) as u32;
     for i in 0..ITERS {
         let j = (a0 & (Cnv2::mem_size() - 0x10)) >> 4;
-        if i < DEBUG_ITERS {
-            eprintln!("{}: aes j: {:x}", i, j << 4);
-        }
         let cc = mem[j as usize];
         let cc = _mm_aesenc_si128(cc, aa);
         var.shuffle_add(mem, j, bb, aa);
-        if i < DEBUG_ITERS {
-            eprintln!("{}: shuffleadd1: _b={:x?} _a={:x?}", i, bb, aa);
-        }
         mem[j as usize] = _mm_xor_si128(bb, cc);
-        if i < DEBUG_ITERS {
-            eprintln!("{}: after aes:", i);
-            print_line(mem, j);
-        }
         let c0 = _mm_extract_epi64(cc, 0) as u64;
         let c1 = _mm_extract_epi64(cc, 1) as u64;
         let j = ((c0 as u32) & (Cnv2::mem_size() - 0x10)) >> 4;
         let b0 = *(&mem[j as usize] as *const _ as *const u64);
         let b1 = *(&mem[j as usize] as *const _ as *const u64).add(1);
         let b0 = var.pre_mul(b0, c0, c1);
-        if i < DEBUG_ITERS {
-            eprintln!("{}: mul j: {:x}", i, j << 4);
-            eprintln!("{}: post-pre-mul var: {:x?}", i, var);
-        }
         let (lo, hi) = mul64(c0, b0);
         let (lo, hi) = var.post_mul(mem.as_mut_ptr() as *mut _, j << 1, lo, hi);
-        if i < DEBUG_ITERS {
-            eprintln!("{}: ({:x}, {:x}) = tweaked_mul({:x}, {:x})", i, lo, hi, c0, b0);
-        }
         var.shuffle_add(mem, j, bb, aa);
         a0 = a0.wrapping_add(hi as u32) ^ b0 as u32;
         aa = _mm_add_epi64(aa, _mm_set_epi64x(lo as i64, hi as i64));
         mem[j as usize] = aa;
-        if i < DEBUG_ITERS {
-            eprintln!("{}: after mul:", i);
-            print_line(mem, j);
-        }
         var.end_iter(bb);
-        if i < DEBUG_ITERS {
-            eprintln!("{}: _b1={:x?}", i, bb);
-        }
         aa = _mm_xor_si128(aa, _mm_set_epi64x(b1 as i64, b0 as i64));
         bb = cc;
     }
